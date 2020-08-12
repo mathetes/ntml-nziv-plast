b394f07c0bec18692658a8a4bde4abaa2637a899
hivensil <hivensil@gmail.com>
2020-08-12 15:17:37 +0700

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-import jQuery from "jquery";
-import popper from "popper.js";
+import {$,jQuery} from "jquery";
+//import popper from "popper.js";
 import bootstrap from "bootstrap";
 
-jQuery(function() {
-  jQuery("body").css("color", "blue");
-});
+//jQuery(function() {
+//  jQuery("body").css("color", "blue");
+//});
