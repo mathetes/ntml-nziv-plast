81eeada21396dcd23d0b3a02cbd2f588dace8d72
Владимир Иванович Шевчук <@nziv.nso>
2020-08-28 12:31:45 +0700

    fixed mask input for field phone

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ import {$,jQuery} from "jquery";
 //import popper from "popper.js";
 import bootstrap from "bootstrap";
 
-var Inputmask = require('inputmask');
+import Inputmask from "inputmask";
+//var Inputmask = require('inputmask');
 
 //jQuery(function() {
 //  jQuery("body").css("color", "blue");
